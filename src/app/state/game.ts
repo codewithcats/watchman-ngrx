898ef7680673c1e9760d6ca21a6a24544fe268a4
Tanawat Tassana <amp.tanawat@gmail.com>
2017-07-23 10:41:34 +0700

    Add start/stop action types

--- src/app/state/game.ts
+++ src/app/state/game.ts
@@ -2,15 +2,20 @@ import {Action} from './state';
 
 interface GameState {
   expected: number;
+  start: number;
+  stop: number;
 }
 
 export const EXPECTED_CHANGE = 'game/EXPECTED_CHANGE';
+export const START_CHANGE = 'game/START_CHANGE';
+export const STOP_CHANGE = 'game/STOP_CHANGE';
 
 export function gameReducer(state: GameState, action: Action): GameState {
   switch (action.type) {
+    case START_CHANGE:
+    case STOP_CHANGE:
     case EXPECTED_CHANGE:
-      const {expected} = action.payload;
-      return Object.assign({}, state, {expected});
+      return Object.assign({}, state, action.payload);
     default: return state;
   }
 }
